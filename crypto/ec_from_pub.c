230769835dbe8d22ce50ef6b309f1b013e5756ee
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-17 17:19:51 -0500

    betty documentation

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -13,11 +13,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
 	if (!pub)
 		return (NULL);
-
 	key = EC_KEY_new_by_curve_name(NID_secp256k1);
 	if (!key)
 		return (NULL);
-
 	group = (EC_GROUP *)EC_KEY_get0_group(key);
 	if (!group)
 	{
@@ -31,21 +29,18 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		EC_KEY_free(key);
 		return (NULL);
 	}
-
 	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL))
 	{
 		EC_POINT_free(point);
 		EC_KEY_free(key);
 		return (NULL);
 	}
-
 	if (!EC_KEY_set_public_key(key, point))
 	{
 		EC_POINT_free(point);
 		EC_KEY_free(key);
 		return (NULL);
 	}
-
 	EC_POINT_free(point);
 	return (key);
 }
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -21,34 +21,35 @@
 /*task 2*/
 #define EC_PUB_LEN 65
 
-/**
-# include <openssl/sha.h>
-# include <stdint.h>
+/*
+*# include <openssl/sha.h>
+* # include <stdint.h>
 
-# define EC_CURVE   NID_secp256k1
+*# define EC_CURVE   NID_secp256k1
 
-EC_KEY public key octet string length (using 256-bit curve)
-#define EC_PUB_LEN 65
-Maximum signature octet string length (using 256-bit curve)
-#define SIG_MAX_LEN    72
+* EC_KEY public key octet string length (using 256-bit curve)
+* #define EC_PUB_LEN 65
+* Maximum signature octet string length (using 256-bit curve)
+* #define SIG_MAX_LEN    72
 
-# define PRI_FILENAME   "key.pem"
-# define PUB_FILENAME   "key_pub.pem"
+* # define PRI_FILENAME   "key.pem"
+* # define PUB_FILENAME   "key_pub.pem"
 
 
-struct sig_s - EC Signature structure
-sig: Signature buffer. The whole space may not be used
-len: Actual signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
+* struct sig_s - EC Signature structure
+* sig: Signature buffer. The whole space may not be used
+* len: Actual signature size. Can't exceed SIG_MAX_LEN,
+* therefore stored on a byte
 
-typedef struct sig_s
-{
+* typedef struct sig_s
+* {
 
-@sig must stay first, so we can directly use the structure as
- an array of char
-    uint8_t     sig[SIG_MAX_LEN];
-    uint8_t     len;
-} sig_t;
- */
+* @sig must stay first, so we can directly use the structure as
+* an array of char
+*    uint8_t     sig[SIG_MAX_LEN];
+*    uint8_t     len;
+*} sig_t;
+*/
 
 /*prototypes*/
 uint8_t *sha256(int8_t const *s, size_t len,

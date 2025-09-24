bab07b2466c7dadd2b2c942b07b3250974d08398
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-24 13:33:45 -0500

    returns verification, took a while to research, compiled

---
+++ crypto/ec_verify.c
@@ -0,0 +1,22 @@
+#include <openssl/ecdsa.h>
+#include <string.h>
+#include "hblk_crypto.h"
+
+/**
+ * ec_verify - verifies the signature of a given set of bytes
+ * @key: pointer to EC KEY struct with public key
+ * @msg: pointer to message bytes
+ * @msglen: length of message
+ * @sig: pointer to struct with signature
+ * Return: 1 if valid, otherwise 0
+ */
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+	size_t msglen, sig_t const *sig)
+	{
+		if (!key || !msg || !sig || !sig->sig || sig->len == 0)
+			return (0);
+
+		return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
+			(EC_KEY *)key));
+	}
+	

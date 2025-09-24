fdcdee5b39964d75a3a1734cb7fcdb57ec7d3f9b
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-24 18:36:18 -0500

    added return

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -12,11 +12,12 @@
  */
 int ec_verify(EC_KEY const *key, uint8_t const *msg,
 	size_t msglen, sig_t const *sig)
-	{
-		if (!key || !msg || !sig || !sig->sig || sig->len == 0)
-			return (0);
+{
+	if (!key || !msg || !sig || !sig->sig || sig->len == 0)
+		return (0);
 
-		return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
-			(EC_KEY *)key));
-	}
-	
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
+		(EC_KEY *)key));
+
+	return (1);
+}

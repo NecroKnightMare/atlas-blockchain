5a7da9c35af75f70224547be37ab83269ad8df4c
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-23 19:12:29 -0500

    pointer to sign using ECDSA, compiled

---
+++ crypto/ec_sign.c
@@ -0,0 +1,42 @@
+#include <openssl/evp.h>
+#include <openssl/evp.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hblk_crypto.h"
+
+/**
+ * ec_sign - sign a message using ECDSA
+ * @key: EC key to sign with
+ * @msg: Message to sign
+ * @msglen: Length of the message
+ * @sig: Pointer to the signature structure
+ *
+ * Return: 1 on success, 0 on failure
+ */
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+size_t msglen, sig_t *sig)
+{
+
+	unsigned int sig_len;
+
+    if (!key || !msg || !sig)
+        return (NULL);
+
+    sig->sig = NULL;
+	sig->len = 0;
+
+	sig_len = ECDSA_size((EC_KEY *)key);
+	sig->sig = malloc(sig_len);
+
+	if (!sig->sig)
+		return (NULL);
+
+	if (!ECDSA_sign(0, msg, msglen, sig->sig, &sig_len, (EC_KEY *)key))
+	{
+		free(sig->sig);
+		sig->sig = NULL;
+		return (NULL);
+	}
+	sig->len = sig_len;
+	return (sig->sig);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -25,6 +25,14 @@
 #define PUB_FILENAME "key_pub.pem"
 #define PRI_FILENAME "key.pem"
 
+/*task 6*/
+typedef struct sig_s
+{
+	uint8_t *sig;
+	uint32_t len;
+} sig_t;
+
+
 
 /*
 *# include <openssl/sha.h>
@@ -64,6 +72,8 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+size_t msglen, sig_t *sig);
 
 
 #endif /* HBLK_CRYPTO_H */

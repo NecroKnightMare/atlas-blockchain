f3cad958a59e300be820f195c7dfb381ed1a00f6
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-07 14:01:58 -0500

    rearranged files again hopefully this fixes it

---
+++ blockchain/blockchain.h
@@ -0,0 +1,45 @@
+#ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include "llist.h"
+#include <openssl/sha.h>
+
+#define BLOCKCHAIN_DATA_MAX 1024
+
+/*block info struct*/
+typedef struct block_info_s
+{
+	uint32_t index;
+	uint32_t difficulty;
+	uint64_t timestamp;
+	uint64_t nonce;
+	uint8_t prev_hash[SHA256_DIGEST_LENGTH];
+} block_info_t;
+
+/*Block data*/
+typedef struct block_s
+{
+	int8_t buffer[BLOCKCHAIN_DATA_MAX];
+	size_t len;
+} block_data_t;
+
+/*Block struct*/
+typedef struct block_s
+{
+	block_info_t info;
+	block_data_t data;
+	uint8_t hash[SHA256_DIGEST_LENGTH];
+} block_t;
+
+/*Blockchain struct*/
+typedef struct blockchain_s
+{
+	llist_t *chain;
+} blockchain_t;
+
+blockchain_t *blockchain_create(void);
+
+
+#endif // BLOCKCHAIN_H
---
+++ blockchain/blockchain_create.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <string.h>
+#include "blockchain.h"
+
+/**
+ * blockchain_create - Creates a new blockchain and inits the genesis block
+ * Return: pointer to the new blockchain or NULL if failed
+ */
+blockchain_t *blockchain_create(void)
+{
+	blockchain_t *blockchain;
+	block_t *genesis;
+	static const block_t genesis = {
+		.info = {
+			.index = 0,
+			.difficulty = 0,
+			.timestamp = 1537578000,
+			.nonce = 0,
+			.prev_hash = {0}
+		},
+		.data = {
+			.buffer = "Holberton School",
+			.len = 16
+		},
+		.hash = {
+			0xc5, 0x2c, 0x26, 0xc8, 0xb5, 0x46, 0x16, 0x39, 0x63, 0x5d, 0x8e,
+			0xdf, 0x2a, 0x97, 0xd4, 0x8d, 0x0c, 0x8e, 0x00, 0x09, 0xc8, 0x17,
+			0xf2, 0xb1, 0xd3, 0xd7, 0xff, 0x2f, 0x04, 0x51, 0x58, 0x03
+		}/* c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803 */
+	}
+};
+	blockchain = malloc(sizeof(*blockchain));
+	if (!blockchain)
+		return (NULL);
+	blockchain->chain = llist_create(MT_SUPPORT_TRUE);
+	if (!blockchain->chain)
+	{
+		free(blockchain);
+		return (NULL);
+	}
+	genesis = malloc(sizeof(*genesis));
+	if (!genesis)
+	{
+		llist_destroy(blockchain->chain, 1, NULL);
+		free(blockchain);
+		return (NULL);
+	}
+	memcpy(genesis, &genesis_block, sizeof(block_t));
+	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_REAR) == -1)
+	{
+		free(genesis);
+		llist_destroy(blockchain->chain, 1, NULL);
+		free(blockchain);
+		return (NULL);
+	}

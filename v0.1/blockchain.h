ff06b91542e950cba459b11b37a8f9d741c0321d
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-06 20:09:59 -0500

    file 1 added with header struct and added directory copied from slack for llist header

--- v0.1/blockchain.h
+++ v0.1/blockchain.h
@@ -3,6 +3,19 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include "llist.h"
+#include <openssl/sha.h>
+
+#define BLOCKCHAIN_DATA_MAX 1024
+
+/*Block struct*/
+typedef struct block_s
+{
+	blocki_info_t info;
+	block_data_t data;
+	uint8_t hash[SHA256_DIGEST_LENGTH];
+} block_t;
+
 
 blockchain_t *blockchain_create(void);
 

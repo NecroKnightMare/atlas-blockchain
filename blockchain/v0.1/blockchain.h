ffdcc6ba380a527a826976d47db967b053cdad36
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-08 18:03:19 -0500

    compiled linked file in create file for genesis block

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -8,17 +8,9 @@
 #include <openssl/sha.h>
 
 #define BLOCKCHAIN_DATA_MAX 1024
-#define GENESIS_INDEX        0
-#define GENESIS_DIFFICULTY   0
-#define GENESIS_TIMESTAMP    1537578000
-#define GENESIS_NONCE        0
-#define GENESIS_PREV_HASH    {0}
-#define GENESIS_DATA         "Holberton School"
-#define GENESIS_DATA_LEN     16
-#define GENESIS_HASH         \
-    { 0xc5, 0x2c, 0x26, 0xc8, 0xb5, 0x46, 0x16, 0x39, 0x63, 0x5d, 0x8e, \
-      0xdf, 0x2a, 0x97, 0xd4, 0x8d, 0x0c, 0x8e, 0x00, 0x09, 0xc8, 0x17, \
-      0xf2, 0xb1, 0xd3, 0xd7, 0xff, 0x2f, 0x04, 0x51, 0x58, 0x03 }
+
+extern const block_t _genesis;
+
 
 /*block info struct*/
 typedef struct block_info_s
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -2,21 +2,6 @@
 #include <string.h>
 #include "blockchain.h"
 
-static const block_t genesis_block = {
-    .info = {
-        .index = GENESIS_INDEX,
-        .difficulty = GENESIS_DIFFICULTY,
-        .timestamp = GENESIS_TIMESTAMP,
-        .nonce = GENESIS_NONCE,
-        .prev_hash = GENESIS_PREV_HASH
-    },
-    .data = {
-        .buffer = GENESIS_DATA,
-        .len = GENESIS_DATA_LEN
-    },
-    .hash = GENESIS_HASH
-};
-
 
 /**
  * blockchain_create - Creates a new blockchain and inits the genesis block
@@ -43,7 +28,7 @@ blockchain_t *blockchain_create(void)
 		free(blockchain);
 		return (NULL);
 	}
-	memcpy(genesis, &genesis_block, sizeof(block_t));
+	memcpy(genesis, &_genesis, sizeof(block_t));
 	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_REAR) == -1)
 	{
 		free(genesis);

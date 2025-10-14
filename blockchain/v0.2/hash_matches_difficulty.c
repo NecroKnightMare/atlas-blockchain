c89cbfb5a8342808981764cbab214fce734541e6
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-14 14:02:19 -0500

    test runs are correct, betty fixes and on to the next

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -11,14 +11,14 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 	uint32_t difficulty)
 {
-    uint32_t i, bits;
+	uint32_t i, bits;
 
-    if (!hash || difficulty > SHA256_DIGEST_LENGTH * 8)
-        return (0);
+	if (!hash || difficulty > SHA256_DIGEST_LENGTH * 8)
+		return (0);
 
-    for (i = 0; i < SHA256_DIGEST_LENGTH && difficulty > 0; i++)
-    {
-        if (difficulty >= 8)
+	for (i = 0; i < SHA256_DIGEST_LENGTH && difficulty > 0; i++)
+	{
+		if (difficulty >= 8)
 		{
 			if (hash[i] != 0)
 				return (0);
@@ -32,5 +32,5 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 			difficulty = 0;
 		}
 	}
-    return (1);
+	return (1);
 }

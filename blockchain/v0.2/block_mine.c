1886eda49059978b924b5658b5f96a361ce2f846
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 14:01:02 -0500

    removed do and replaced with while 1

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -5,18 +5,18 @@
  * block_mine - mines block by finding hash that matches difficulty
  * @block: pointer to block mined
  */
-
 void block_mine(block_t *block)
 {
 	if (!block)
 		return;
 	block->info.nonce = 0;
 
-	do {
+	while (1)
+	{
 		block_hash(block, block->hash);
 		if (hash_matches_difficulty(block->hash,
 			block->info.difficulty))
 			break;
 		block->info.nonce++;
-	} while (1);
+	}
 }

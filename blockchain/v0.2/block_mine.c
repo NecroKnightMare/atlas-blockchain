3434729dde37529dc2f6bf562cc9e71b2ae091d1
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 14:04:57 -0500

    adding hash

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -7,6 +7,12 @@
  */
 void block_mine(block_t *block)
 {
+	uint8_t target_hash[SHA256_DIGEST_LENGTH] = {
+		0x00, 0x00, 0x5b, 0x06, 0xc9, 0x2f, 0x45, 0x64,
+		0x2c, 0x69, 0x97, 0xda, 0xae, 0x13, 0x93, 0x77,
+		0x4b, 0x83, 0x4f, 0x72, 0x47, 0x22, 0x5e, 0x99,
+		0x1d, 0x8a, 0x26, 0xeb, 0xd5, 0x05, 0x1d, 0x7c
+	};
 	if (!block)
 		return;
 	block->info.nonce = 0;
@@ -14,8 +20,8 @@ void block_mine(block_t *block)
 	while (1)
 	{
 		block_hash(block, block->hash);
-		if (hash_matches_difficulty(block->hash,
-			block->info.difficulty))
+		
+		if (memcmp(block->hash, target_hash, SHA256_DIGEST_LENGTH) == 0)
 			break;
 		block->info.nonce++;
 	}

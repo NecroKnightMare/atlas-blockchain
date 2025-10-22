3c5ea84c66554771f1456dbedf8f2c41be7f80be
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 14:15:04 -0500

    removed adjus and replaced with last block in chain if statement

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -28,15 +28,13 @@ uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 	adjust = llist_get_node_at(blockchain->chain, size -
 		DIFFICULTY_ADJUSTMENT_INTERVAL);
 
-	if (!adjust)
-		return (last_block->info.difficulty);
 	expected_time = BLOCK_GENERATION_INTERVAL *
 	DIFFICULTY_ADJUSTMENT_INTERVAL;
 	actual_time = last_block->info.timestamp -
 	adjust->info.timestamp;
 	
 	if (actual_time < expected_time / 2)
-		return (adjust->info.difficulty + 1);
+		return (last_block->info.difficulty + 1);
 	else if (actual_time > expected_time * 2)
 		return (last_block->info.difficulty > 0 ?
 			last_block->info.difficulty - 1 : 0);

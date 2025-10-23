3e343a7bc39e6996678a398e7e57f158bf862ad8
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 21:56:49 -0500

    if this isn't gonna work I'll try brute forcing next

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -16,8 +16,7 @@ void block_mine(block_t *block)
 	while (1)
 	{
 		block_hash(block, block->hash);
-		if (memcmp(block->hash, _genesis.hash, SHA256_DIGEST_LENGTH) == 0 ||
-			hash_matches_difficulty(block->hash, block->info.difficulty))
+		if (memcmp(block->hash, (uint8_t[SHA256_DIGEST_LENGTH]){0}, block->info.difficulty / 8) == 0)
 			break;
 		block->info.nonce++;
 	}

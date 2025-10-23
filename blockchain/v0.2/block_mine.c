2303b9bfb702c1284f21d072ff8fca55d307add6
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 21:42:20 -0500

    added time to reflect the time

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,5 +1,6 @@
 #include "blockchain.h"
 #include <string.h>
+#include <time.h>
 
 /**
  * block_mine - mines block by finding hash that matches difficulty
@@ -13,6 +14,7 @@ void block_mine(block_t *block)
 
 	while (1)
 	{
+		block->info.timestamp = (uint64_t)time(NULL);
 		block_hash(block, block->hash);
 		if (hash_matches_difficulty(block->hash,
 			block->info.difficulty))

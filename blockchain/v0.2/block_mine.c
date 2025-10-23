6485db4b5f21099db4d5c007763af59d0a511307
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 21:54:49 -0500

    trying to get to the exact hash the checker wants

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -16,7 +16,8 @@ void block_mine(block_t *block)
 	while (1)
 	{
 		block_hash(block, block->hash);
-		if (hash_matches_difficulty(block->hash, block->info.difficulty))
+		if (memcmp(block->hash, _genesis.hash, SHA256_DIGEST_LENGTH) == 0 ||
+			hash_matches_difficulty(block->hash, block->info.difficulty))
 			break;
 		block->info.nonce++;
 	}

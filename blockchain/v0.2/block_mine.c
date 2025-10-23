1e45606dcdd393d664afb1182676aa393d6b6f06
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 22:15:05 -0500

    reverted again

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,20 +1,22 @@
 #include "blockchain.h"
 #include <string.h>
-#include <time.h>
 
 /**
  * block_mine - mines block by finding hash that matches difficulty
  * @block: pointer to block mined
  */
+
 void block_mine(block_t *block)
 {
 	if (!block)
 		return;
 	block->info.nonce = 0;
 
-	do
-	{
+	do {
 		block_hash(block, block->hash);
+		if (hash_matches_difficulty(block->hash,
+			block->info.difficulty))
+			break;
 		block->info.nonce++;
-	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
+	} while (1);
 }

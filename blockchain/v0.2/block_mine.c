46d6a2619b49f0c50893c29c4e2fc8814c16ea82
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 21:45:59 -0500

    reverted

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -12,10 +12,13 @@ void block_mine(block_t *block)
 		return;
 	block->info.nonce = 0;
 
-	do
+	while (1)
 	{
 		block->info.timestamp = (uint64_t)time(NULL);
 		block_hash(block, block->hash);
+		if (hash_matches_difficulty(block->hash,
+			block->info.difficulty))
+			break;
 		block->info.nonce++;
-	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
+	}
 }

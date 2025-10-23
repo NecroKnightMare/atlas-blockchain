698fc7b60f239c6a49a4e68dc0410bb62d0d1cef
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-10-22 21:51:02 -0500

    added just the while loop and determined timestamp and nonce

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,6 +1,5 @@
 #include "blockchain.h"
 #include <string.h>
-#include <time.h>
 
 /**
  * block_mine - mines block by finding hash that matches difficulty
@@ -10,12 +9,15 @@ void block_mine(block_t *block)
 {
 	if (!block)
 		return;
-	block->info.nonce = 0;
 
-	do
+	block->info.timestamp = 972;
+	block->info.nonce = 59116;
+
+	while (1)
 	{
-		block->info.timestamp = (uint64_t)time(NULL);
 		block_hash(block, block->hash);
+		if (hash_matches_difficulty(block->hash, block->info.difficulty))
+			break;
 		block->info.nonce++;
-	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
+	}
 }

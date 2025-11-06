0484915014091c936ea469e3a914fd7ac87dd654
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-11-05 23:57:02 -0600

    added files to sub

---
+++ blockchain/v0.3/blockchain_destroy.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "blockchain.h"
+
+/**
+ * blockchain_destroy - frees a blockchain and all its blocks
+ * @blockchain: pointer to the blockchain to delete
+ */
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+
+	llist_destroy(blockchain->chain, 1, (node_dtor_t)block_destroy);
+	free(blockchain);
+}

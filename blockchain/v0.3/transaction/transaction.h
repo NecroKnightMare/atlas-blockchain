aee1228e81a42a624aae84d8a87fe2179718df03
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-11-05 19:18:59 -0600

    removed some more functions

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -63,10 +63,6 @@ typedef struct tx_in_s {
     sig_t sig;
 } tx_in_t;
 
-/*print functions*/
-int _transaction_print_loop(transaction_t const *transaction, unsigned int indent);
-int _transaction_print_brief_loop(transaction_t const *transaction, unsigned int indent);
-
 /*v0.3 prototypes*/
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
 unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH], uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);

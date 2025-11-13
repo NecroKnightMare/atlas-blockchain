57024eec979d3c79e35dd84c10c6a22c57dec327
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-11-12 19:43:03 -0600

    put function into genesis file

--- blockchain/v0.3/provided/_genesis.c
+++ blockchain/v0.3/provided/_genesis.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include "blockchain.h"
 
 block_t const _genesis = {
@@ -17,3 +19,25 @@ block_t const _genesis = {
 	/* hash */
 	/* c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803 */
 };
+
+/**
+ * genesis_block - Allocates and returns a copy of the predefined genesis block
+ * Return: pointer to a newly allocated genesis block, NULL on failure
+ */
+block_t *genesis_block(void)
+{
+	block_t *block = calloc(1, sizeof(block_t));
+	if (!block)
+		return (NULL);
+
+	memcpy(block, &_genesis, sizeof(block_t));
+
+	block->transactions = llist_create(MT_SUPPORT_FALSE);
+	if (!block->transactions)
+	{
+		free(block);
+		return (NULL);
+	}
+
+	return (block);
+}

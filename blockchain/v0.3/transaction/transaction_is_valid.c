89842ebe3fe0215026758a610db56a1a56b3bb69
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-11-12 22:09:20 -0600

    used appropriate signs.

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -1,8 +1,7 @@
 #include "blockchain.h"
-#include <string.h>
 #include <openssl/ec.h>
-#include <openssl/sha.h>
-#include "transaction.h"
+#include <openssl/obj_mac.h>
+include "transaction.h"
 
 /**
  * transaction_is_valid - verifies a transaction’s integrity and authenticity
@@ -17,7 +16,8 @@ int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent)
 	tx_out_t *tx_out;
 	unspent_tx_out_t *unspent;
 	EC_KEY *pub_key;
-	size_t i, j;
+	int i, j;
+	int input_count, output_count, unspent_count;
 	uint32_t total_in = 0, total_out = 0;
 
 	if (!transaction || !all_unspent)
@@ -27,14 +27,18 @@ int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent)
 	if (memcmp(hash_buf, transaction->id, SHA256_DIGEST_LENGTH) != 0)
 		return (0);
 
-	for (i = 0; i < llist_size(transaction->inputs); i++)
+	input_count = llist_size(transaction->inputs);
+	output_count = llist_size(transaction->outputs);
+	unspent_count = llist_size(all_unspent);
+
+	for (i = 0; i < input_count; i++)
 	{
 		tx_in = llist_get_node_at(transaction->inputs, i);
 		if (!tx_in)
 			return (0);
 
 		unspent = NULL;
-		for (j = 0; j < llist_size(all_unspent); j++)
+		for (j = 0; j < unspent_count; j++)
 		{
 			unspent_tx_out_t *u = llist_get_node_at(all_unspent, j);
 			if (!u)
@@ -51,7 +55,6 @@ int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent)
 
 		if (!unspent)
 			return (0);
-
 		pub_key = ec_from_pub(unspent->out.pub);
 		if (!pub_key || !ec_verify(pub_key, transaction->id, SHA256_DIGEST_LENGTH, &tx_in->sig))
 		{
@@ -64,8 +67,7 @@ int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent)
 		total_in += unspent->out.amount;
 	}
 
-
-	for (i = 0; i < llist_size(transaction->outputs); i++)
+	for (i = 0; i < output_count; i++)
 	{
 		tx_out = llist_get_node_at(transaction->outputs, i);
 		if (!tx_out)
